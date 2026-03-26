6940348e5187073a0b39aa974678bcde507520ef
James K <james.kelleher.3345@gmail.com>
2026-03-26 13:28:35 -0400

    update base

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     root: '.',
+    base: '/jameskelleher/',
     build: {
         rollupOptions: {
             input: './index.html',
